9653bac69b80bca1de249fb3d2b2a698e6f51589
Seulji-jo <joseuling@gmail.com>
2024-02-25 20:59:44 +0900

    reset to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <button>
-        <a
-          href={`https://accounts.google.com/o/oauth2/auth?client_id=${`448940645981-3kp181mdmltl6bfltpvvso2v1vlt8h6e.apps.googleusercontent.com`}&redirect_uri=http://localhost:3000/auth/google&scope=https://www.googleapis.com/auth/youtube&response_type=token`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          google login
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-      </button>
-    </div>
-  );
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
